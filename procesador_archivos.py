a94ec8970ca9708a88365fce5ca18b9424a96b73
Mauricio Zamora <mauricio.zamora@gmail.com>
2020-02-19 23:20:45 -0600

    Incorporación de procesamiento masivo

--- procesador_archivos.py
+++ procesador_archivos.py
@@ -15,10 +15,14 @@ def procesar_lineas(lineas, sufijos):
         sublineas = linea[:-1].split(' ')
         for sufijo in sufijos:
             tmp = sublineas[0] + sufijo
-            if len(sublineas) == 2:
-                salida.append(tmp + ' ' + str(sublineas[1]))
+            if len(sublineas) > 1:
+                if len(sublineas) == 2:
+                    salida.append(tmp + ' ' + str(sublineas[1]))
+                else:
+                    salida.append(tmp + ' ' + str(sublineas[2]))
             else:
-                salida.append(tmp + ' ' + str(sublineas[2]))
+                salida.append(tmp)
+
     return salida
 
 
@@ -36,6 +40,7 @@ def listar_archivos_texto(direcotorio_base):
 
 
 def procesar_archivo_entrenamiento(archivo_original, archivo_procesado):
+    print('Procesando: ' + archivo_original)
     lineas = leer_archivo(archivo_original)
     ls = procesar_lineas(lineas, ['_canf', '_rot90', '_rot180', '_rot270', '_tranx100y100'])
     escribir_archivo_lineas(archivo_procesado, ls)
@@ -56,7 +61,12 @@ def main():
     # fs = listar_archivos_texto('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/')
     procesar_archivos_entrenamiento('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/',
                                     '/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Main/')
-
+    procesar_archivos_entrenamiento('/home/mauricio/Escritorio/tools_dataset/ImageSets/Action/',
+                                    '/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Action/')
+    procesar_archivos_entrenamiento('/home/mauricio/Escritorio/tools_dataset/ImageSets/Layout/',
+                                    '/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Layout/')
+    procesar_archivos_entrenamiento('/home/mauricio/Escritorio/tools_dataset/ImageSets/Segmentation/',
+                                    '/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Segmentation/')
 
 if __name__ == '__main__':
     main()
