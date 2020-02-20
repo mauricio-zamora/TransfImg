be763cb6a6c109e22c4f86b76af6308a07d8ee33
Mauricio Zamora <mauricio.zamora@gmail.com>
2020-02-19 19:34:44 -0600

    Incorporación de procesamiento masivo

--- procesador_archivos.py
+++ procesador_archivos.py
@@ -1,4 +1,6 @@
 import glob
+import os
+
 
 def leer_archivo(archivo):
     f = open(archivo, "r")
@@ -33,12 +35,28 @@ def listar_archivos_texto(direcotorio_base):
     return archivos
 
 
+def procesar_archivo_entrenamiento(archivo_original, archivo_procesado):
+    lineas = leer_archivo(archivo_original)
+    ls = procesar_lineas(lineas, ['_canf', '_rot90', '_rot180', '_rot270', '_tranx100y100'])
+    escribir_archivo_lineas(archivo_procesado, ls)
+
+
+def procesar_archivos_entrenamiento(directorio_base, directorio_destino):
+    nombres_archivos = listar_archivos_texto(directorio_base)
+    for nombre_archivo in nombres_archivos:
+        nombre_completo_archivo_origen = os.path.join(directorio_base, nombre_archivo)
+        nombre_completo_archivo_destino = os.path.join(directorio_destino, nombre_archivo)
+        procesar_archivo_entrenamiento(nombre_completo_archivo_origen, nombre_completo_archivo_destino)
+
+
 def main():
-    #lineas = leer_archivo('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/screw_train.txt')
-    #ls = procesar_lineas(lineas,['_canf','_rot90','_rot180','_rot270','_tranx100y100'])
-    #escribir_archivo_lineas('/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Main/screw_train.txt', ls)
-    fs = listar_archivos_texto('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/')
-    print(fs)
+    # lineas = leer_archivo('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/screw_train.txt')
+    # ls = procesar_lineas(lineas,['_canf','_rot90','_rot180','_rot270','_tranx100y100'])
+    # escribir_archivo_lineas('/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Main/screw_train.txt', ls)
+    # fs = listar_archivos_texto('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/')
+    procesar_archivos_entrenamiento('/home/mauricio/Escritorio/tools_dataset/ImageSets/Main/',
+                                    '/home/mauricio/Escritorio/tools_dataset/nuevo_ds/ImageSets/Main/')
+
 
 if __name__ == '__main__':
     main()
